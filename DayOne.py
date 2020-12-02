cc577613a3dc991933c90829066a7ae99965006f
Carson <carson.kegley@gmail.com>
2020-12-02 14:59:39 -0500

    First two days

---
+++ DayOne.py
@@ -0,0 +1,25 @@
+f = open("DayOneData.txt", "r")
+content = f.read()
+content_list = content.split("\n")
+content_list = [int(i) for i in content_list]
+list.sort(content_list)
+
+returnVal = 0  
+
+def recurse(findFor):
+    for val in content_list:
+        valInverse = findFor - val
+        if content_list.count(valInverse) != 0:
+            return  valInverse * val
+            
+
+for val in content_list:
+        valInverse = 2020 - val
+        temp = recurse(valInverse)
+        if type(temp) == int  and temp != 0:
+            returnVal = temp * val
+            break
+
+print(returnVal)
+
+
---
+++ DayTwo.py
@@ -0,0 +1,33 @@
+from operator import xor
+f = open("DayTwoData.txt", "r")
+content = f.read()
+content_list = content.split("\n")
+cleanedInputs = []
+upperAndLowerBounds = []
+searchLetter = []
+searchString = []
+validPasswords = 0
+
+for line in content_list:
+    cleanedInputs.append(line.split())
+
+for index in range(0, len(cleanedInputs)):
+    upperAndLowerBounds.append(cleanedInputs[index][0].split("-"))
+    searchLetter.append(cleanedInputs[index][1][0])
+    searchString.append(cleanedInputs[index][2])
+
+#Solution one commented out to save space and time
+# for index in range(0, len(cleanedInputs)):
+#     temp = searchString[index].count(searchLetter[index])
+#     if temp <= int(upperAndLowerBounds[index][1]) and temp >= int(upperAndLowerBounds[index][0]):
+#         validPasswords = 1 + validPasswords
+
+for index in range(0, len(cleanedInputs)):
+    letter = searchLetter[index]
+    upper = int(upperAndLowerBounds[index][1])-1
+    lower = int(upperAndLowerBounds[index][0])-1
+    if upper <= len(searchString[index]) -1 :
+        if xor(bool(searchString[index][upper] == letter), bool(searchString[index][lower] == letter)):
+            validPasswords = validPasswords + 1
+
+print(validPasswords)
